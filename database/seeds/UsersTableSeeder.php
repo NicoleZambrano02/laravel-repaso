cb892aa13ded41b85fb81c8763cd698a32cf20d3
Nicole Zambrano <znicole473@gmail.com>
2020-05-18 17:07:04 -0500

    #12 relationships

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Category;
 use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -13,26 +14,37 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //Vaciar la tabla
+        // Vaciar la tabla
         User::truncate();
 
-        //la misma clave para todos los usuarios
         $faker = \Faker\Factory::create();
-        $password = Hash::make('123456');
 
-        //Crear artículos ficticios en la tabla
+        // Crear la misma clave para todos los usuarios
+        // conviene hacerlo antes del for para que el seeder
+        // no se vuelva lento.
+        $password = Hash::make('123456');
         User::create([
             'name' => 'Administrador',
             'email' => 'admin@prueba.com',
             'password' => $password,
         ]);
 
-        for ($i = 0; $i < 10; $i++){
-            User::create([
-                'name' => $faker -> name,
-                'email' => $faker -> email,
-                'password' => $faker -> password,
+        // Generar algunos usuarios para nuestra aplicacion
+        for ($i = 0; $i < 10; $i++) {
+            $user = User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
             ]);
+
+            $user->categories()->saveMany(
+                $faker->randomElements(
+                    array(
+                        Category::find(1),
+                        Category::find(2),
+                        Category::find(3)
+                    ), $faker->numberBetween(1, 3), false)
+            );
         }
     }
 }
